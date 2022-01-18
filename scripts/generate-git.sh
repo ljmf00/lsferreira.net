de18842f1ef94d1d9cf92fa08c6dbc05f8543e41
Luís Ferreira <contact@lsferreira.net>
2022-01-18 16:22:59 +0000

    fix(generation): add info/refs on git bare repository
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- scripts/generate-git.sh
+++ scripts/generate-git.sh
@@ -9,7 +9,10 @@
 	git clone --bare https://github.com/ljmf00/lsferreira.net "$TEMP_DIR"
 	mv "$TEMP_DIR"/* .
     fi
-    GIT_DIR=. git worktree add file master
+    ( export GIT_DIR=.
+	git worktree add file master
+	git update-server-info
+    )
 
     stagit .
     cp log.html index.html
